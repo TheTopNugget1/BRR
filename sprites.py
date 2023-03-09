34bacdb05bdead1c120802cda280c4996b9422b5
TheTopNugget1 <126112786+TheTopNugget1@users.noreply.github.com>
2023-03-09 22:10:10 +1300

    Add files via upload

--- sprites.py
+++ sprites.py
@@ -3,10 +3,9 @@ import pygame
 
 
 # Sprite class tiles
-class Block(pygame.sprite.Sprite):
+class tile(pygame.sprite.Sprite):
     def __int__(self, pos, size):
         super().__int__()
         self.image = pygame.Surface((size, size))
-        self.image.fill('white')
+        self.image.fill('grey')
         self.rect = self.image.get_rect(topleft=pos)
-
